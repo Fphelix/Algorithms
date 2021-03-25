a74fccbe34c2185919e5e66bb088c1a50629041a
Fphelix <77512995+Fphelix@users.noreply.github.com>
2021-03-24 23:22:15 -0400

    Add files via upload

---
+++ AdjacentArray/AdjacentArray/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace AdjacentArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            int[] array = { 3, -2, -2, -5, 7, 3 };
+            int max = 0;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int new_max = array[i] * array[i + 1];
+                if (new_max > max)
+                {
+                    max = new_max;
+
+                }
+            }
+            Console.WriteLine(max); 
+        }
+
+
+
+
+
+
+        //int  currNumber = array[0] * array[1];
+
+        // int product = 0;
+        // int MaxProduct = 0;
+
+        //for ( i = 1; i < array.Length; i++ )
+        // {
+        //     int currNumber = array[0] * array[i];
+        //     product = array[i] * array[i - 1];
+        //      MaxProduct = Math.Max(currNumber, product);
+
+        // }
+        // Console.WriteLine(  MaxProduct);
+    }
+}
