9f316ca02f035e16a8cae78f2cf3bb07c8d41363
Fphelix <77512995+Fphelix@users.noreply.github.com>
2021-03-30 22:21:54 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ContainsDuplicate
+{
+    class Program
+    {
+        
+            static void Main(string[] args)
+            {
+                int[] nums = { 1, 2, 3, 1 };
+
+            }
+
+            public Boolean containsDuplicate(int[] nums)
+            {
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (nums[i] == nums[j])
+
+                            return true;
+
+                    }
+                }
+                return true;
+            }
+        
+    }
+}
